e048e0eaaadbf694e0f3bb6e5b7e7638c812b770
Bert Temme <btemme@hotmail.com>
2019-12-13 17:14:27 +0100

    add .net core testcase

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # spatialite_sample
 
-Sample for reading spatialite database in .NET Framework 4.7
+Sample for reading spatialite database in .NET Framework 4.7 (Project: spatialite_sample_net)
 
-Todo: Get this working in .NET Core
+Todo: Get this working in .NET Core (Project: spatialite_sample_core). 
+
+Error when running this project: "exited with code -1073741819"
 
 
---
+++ src/spatialite_sample_core/Program.cs
@@ -0,0 +1,43 @@
+﻿using spatialite_sample_net;
+using System;
+using System.Data.SQLite;
+
+namespace spatialite_sample_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string db = @"test-2.3.sqlite";
+
+            string connectString = "Data Source=" + db + ";Version=3;";
+            var connection = new SQLiteConnection(connectString);
+            connection.Open();
+            connection.EnableExtensions(true);
+            SpatialiteLoader.Load(connection);
+
+            string sql = "SELECT Name, ST_MINX(geometry), ST_MINY(geometry), ST_MAXX(geometry), ST_MAXY(geometry) FROM Towns ";
+
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(0);
+                        double minX = reader.GetDouble(1);
+                        double minY = reader.GetDouble(2);
+                        double maxX = reader.GetDouble(3);
+                        double maxY = reader.GetDouble(4);
+
+                        Console.WriteLine($"{name}: {minX}, {minY}, {maxX}, {maxY}");
+                    }
+                }
+            }
+
+            connection.Close();
+            connection.Dispose();
+
+        }
+    }
+}
