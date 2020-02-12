5889f38ce27e117cb0f067ad00b84890d0f63296
Bert Temme <btemme@hotmail.com>
2020-02-12 21:55:13 +0100

    add xamarin.forms app to test

---
+++ src/spatialite_sample_core/PointsLoader.cs
@@ -0,0 +1,40 @@
+﻿using spatialite_sample_net;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using Wkx;
+
+namespace spatialite_sample_lib
+{
+    public class PointsLoader
+    {
+        public static List<Point> GetPoints(string db, string sql)
+        {
+            string connectString = "Data Source=" + db + ";Version=3;";
+            var connection = new SQLiteConnection(connectString);
+            connection.Open();
+            connection.EnableExtensions(true);
+
+            #if Windows
+                SpatialiteLoader.Load(connection);
+            #elif OSX || Linux
+                connection.LoadExtension("mod_spatialite");
+            #endif
+
+            var command = new SQLiteCommand(sql, connection);
+            var reader = command.ExecuteReader();
+            var points = new List<Point>(); 
+            while (reader.Read())
+            {
+                var name = reader.GetString(0);
+                var geometry = Geometry.Deserialize<WkbSerializer>((byte[])reader[1]);
+                var point = (Point)geometry;
+                points.Add(point);
+            }
+
+            connection.Close();
+            connection.Dispose();
+
+            return points;
+        }
+    }
+}
--- src/spatialite_sample_core/Program.cs
+++ src/spatialite_sample_core/Program.cs
@@ -1,7 +1,5 @@
-﻿using spatialite_sample_net;
+﻿using spatialite_sample_lib;
 using System;
-using System.Data.SQLite;
-using Wkx;
 
 namespace spatialite_sample_core
 {
@@ -10,32 +8,14 @@ namespace spatialite_sample_core
         static void Main(string[] args)
         {
             string db = @"test-2.3.sqlite";
+            string sql = "SELECT Name, ST_ASBinary(geometry) FROM Towns;";
 
-            string connectString = "Data Source=" + db + ";Version=3;";
-            var connection = new SQLiteConnection(connectString);
-            connection.Open();
-            connection.EnableExtensions(true);
-
-#if Windows
-             SpatialiteLoader.Load(connection);
-#elif OSX || Linux
-             connection.LoadExtension("mod_spatialite");
-#endif
-
-            string sql = "SELECT Name, ST_ASBinary(geometry) FROM Towns ";
-
-            using var command = new SQLiteCommand(sql, connection);
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            var points = PointsLoader.GetPoints(db, sql);
+            foreach(var p in points)
             {
-                var name = reader.GetString(0);
-                var geometry = Geometry.Deserialize<WkbSerializer>((byte[])reader[1]);
-                var point = (Point)geometry;
-                Console.WriteLine($"{name}: {point.X}, {point.Y}");
+                Console.WriteLine(p.X + ", " + p.Y);
             }
-
-            connection.Close();
-            connection.Dispose();
         }
+
     }
 }
